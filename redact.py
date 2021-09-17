b87532781ffc037856fdd7975c514890b634e76b
Danny <danny@kokomo247.com>
2021-09-16 18:45:30 -0700

    Done with Part1

---
+++ redact.py
@@ -0,0 +1,22 @@
+import http.client
+import json
+from re import T
+import sys
+import urllib.parse
+
+foaas = http.client.HTTPSConnection("foaas.com")
+Purgo = http.client.HTTPSConnection("www.purgomalum.com")
+
+foaas_headers = {'Accept' : 'application/json'}
+foaas.request("GET", sys.argv[1], headers=foaas_headers)
+foaas_response = foaas.getresponse()
+target_json = json.loads(foaas_response.read())
+query = urllib.parse.quote(target_json['message'])
+
+Purgo.request("GET", "/service/json?text=" + query)
+Purgo_response = Purgo.getresponse()
+flitered_message = json.loads(Purgo_response.read())
+target_json['message'] = flitered_message['result']
+
+json_str = json.dumps(target_json, indent=4)
+print(json_str)
