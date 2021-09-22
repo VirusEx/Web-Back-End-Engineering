02ab009bdd5564216e72bb8579bc6da13e878eb8
Shelly-ss <72415765+Shelly-ss@users.noreply.github.com>
2021-09-21 23:59:43 -0700

    fix

--- 1.py
+++ 1.py
@@ -1,23 +1,19 @@
-#!/usr/bin/env python3
-
-# Example HTTP server
-#
-# See <https://docs.python.org/3/library/http.server.html> for details
-#
-
 import http.server
 import socketserver
 
 PORT = 8080
 
-
 class ExampleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    # def __init__(self, message, subtitle):
+    #     self.message = message
+    #     self.subtitle = subtitle
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-Type', 'text/html; charset=utf-8')
         self.end_headers()
 
-        payload = '<h1>Hello, world</h1>'
+        payload = '<h1>message</h1>'
         self.wfile.write(payload.encode('utf-8'))
 
 
--- redact.py
+++ redact.py
@@ -27,19 +27,3 @@ json_str = json.dumps(target_json, indent=4)
 print(json_str)
 
 
-class ExampleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/html; charset=utf-8')
-        self.end_headers()
-
-        payload = '<h1></h1>'
-        self.wfile.write(payload.encode('utf-8'))
-
-
-
-
-
-with socketserver.TCPServer(("", PORT), ExampleHTTPRequestHandler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
