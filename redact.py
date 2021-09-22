eee08735b270f02a86baf959c645d38aed69273b
Shelly-ss <72415765+Shelly-ss@users.noreply.github.com>
2021-09-21 23:34:30 -0700

    add the html page

--- redact.py
+++ redact.py
@@ -4,6 +4,11 @@ from re import T
 import sys
 import urllib.parse
 
+import http.server
+import socketserver
+
+PORT = 8080
+
 foaas = http.client.HTTPSConnection("foaas.com")
 Purgo = http.client.HTTPSConnection("www.purgomalum.com")
 
@@ -20,3 +25,21 @@ target_json['message'] = flitered_message['result']
 
 json_str = json.dumps(target_json, indent=4)
 print(json_str)
+
+
+class ExampleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+        payload = '<h1></h1>'
+        self.wfile.write(payload.encode('utf-8'))
+
+
+
+
+
+with socketserver.TCPServer(("", PORT), ExampleHTTPRequestHandler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
